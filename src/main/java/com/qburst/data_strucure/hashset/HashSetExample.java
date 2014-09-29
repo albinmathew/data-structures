2728c72ba6e72205488f33688497685f12dd5ac6
albin <albin@qburst.com>
2014-09-29 20:10:52 +0530

    hash set added

---
+++ src/main/java/com/qburst/data_strucure/hashset/HashSetExample.java
@@ -0,0 +1,29 @@
+package com.qburst.data_strucure.hashset;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+/**
+ * Created by albin on 29/9/14.
+ */
+public class HashSetExample {
+ public static  void main(String[] args){
+     HashSet<Integer> hashSet = new HashSet<Integer>();
+
+     hashSet.add(0);
+     hashSet.add(1);
+     hashSet.add(2);
+     hashSet.add(3);
+     hashSet.add(4);
+     hashSet.add(5);
+
+     Integer min = Collections.min(hashSet);
+     Integer max = Collections.max(hashSet);
+
+     System.out.println("HashSet contains" + hashSet);
+     System.out.println("Size of HashSet " + hashSet.size());
+     System.out.println("Minimum Element of  HashSet is : " + min);
+     System.out.println("Maximum Element of  HashSet is : " + max);
+
+ }
+}
