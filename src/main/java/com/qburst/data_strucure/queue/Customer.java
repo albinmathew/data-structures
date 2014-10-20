451a06c5421e68bb58b0acbb1e4006c6d1e033a8
albin <albin@qburst.com>
2014-10-20 18:07:47 +0530

    Priority queue data structure example

---
+++ src/main/java/com/qburst/data_strucure/queue/Customer.java
@@ -0,0 +1,31 @@
+package com.qburst.data_strucure.queue;
+
+/**
+ * Created by albinmathew on 20/10/14.
+ */
+public class Customer {
+    private String name;
+    private int id;
+
+    public Customer(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
