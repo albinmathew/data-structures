a3327018632846bf057f70c22105d2e368b12612
albin <albin@qburst.com>
2014-11-07 20:31:04 +0530

    quick sort

--- src/main/java/com/qburst/data_strucure/sorts/quick_sort/QuickSort.java
+++ src/main/java/com/qburst/data_strucure/sorts/quick_sort/QuickSort.java
@@ -1,7 +1,59 @@
 package com.qburst.data_strucure.sorts.quick_sort;
 
 /**
- * Created by albinmathew on 7/11/14.
+ * Created by albinmathew on 6/11/14.
  */
 public class QuickSort {
+    private int array[];
+    private int length;
+
+    public void sort(int[] inputArr) {
+
+        if (inputArr == null || inputArr.length == 0) {
+            return;
+        }
+        this.array = inputArr;
+        length = inputArr.length;
+        quickSort(0, length - 1);
+    }
+
+    private void quickSort(int lowerIndex, int higherIndex) {
+
+        int i = lowerIndex;
+        int j = higherIndex;
+        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                exchangeNumbers(i, j);
+                i++;
+                j--;
+            }
+        }
+        if (lowerIndex < j)
+            quickSort(lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(i, higherIndex);
+    }
+
+    private void exchangeNumbers(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String a[]){
+
+        QuickSort sort = new QuickSort();
+        int[] input = {4, 2, 9, 6, 23, 5, 52, 12, 34, 0, 1};
+        sort.sort(input);
+        for(int i:input){
+            System.out.print(i + ",");
+        }
+    }
 }
