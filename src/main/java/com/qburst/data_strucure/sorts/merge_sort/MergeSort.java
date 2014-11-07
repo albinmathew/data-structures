7130664e6305c8df6f534bd5ee8d53f4df53ccdb
albin <albin@qburst.com>
2014-11-07 20:30:30 +0530

    merge sort

--- src/main/java/com/qburst/data_strucure/sorts/merge_sort/MergeSort.java
+++ src/main/java/com/qburst/data_strucure/sorts/merge_sort/MergeSort.java
@@ -1,7 +1,68 @@
 package com.qburst.data_strucure.sorts.merge_sort;
 
 /**
- * Created by albinmathew on 7/11/14.
+ * Created by albinmathew on 6/11/14.
  */
 public class MergeSort {
+
+    private int[] array;
+    private int[] tempMergArr;
+    private int length;
+
+    public static void main(String a[]){
+
+        int[] inputArr = {4, 2, 9, 6, 23, 5, 52, 12, 34, 0, 1};
+        MergeSort mms = new MergeSort();
+        mms.sort(inputArr);
+        for(int i:inputArr){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+
+    public void sort(int inputArr[]) {
+        this.array = inputArr;
+        this.length = inputArr.length;
+        this.tempMergArr = new int[length];
+        doMergeSort(0, length - 1);
+    }
+
+    private void doMergeSort(int lowerIndex, int higherIndex) {
+
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            // sorts the left side of the array
+            doMergeSort(lowerIndex, middle);
+            // sorts the right side of the array
+            doMergeSort(middle + 1, higherIndex);
+            // merge both sides
+            mergeParts(lowerIndex, middle, higherIndex);
+        }
+    }
+
+    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergArr[i] = array[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (tempMergArr[i] <= tempMergArr[j]) {
+                array[k] = tempMergArr[i];
+                i++;
+            } else {
+                array[k] = tempMergArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= middle) {
+            array[k] = tempMergArr[i];
+            k++;
+            i++;
+        }
+
+    }
 }
